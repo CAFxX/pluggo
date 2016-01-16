7151478773da1bdfbdbd8526802a365981686e89
Carlo Alberto Ferraris <cafxx@strayorange.com>
2016-01-16 22:13:03 +0900

    Fix locking mistake

--- plugin.go
+++ plugin.go
@@ -16,12 +16,12 @@ var lock = sync.RWMutex{}
 // for the named extension point. It is illegal to register more than one
 // Factory for the same extension point.
 func Register(name string, f Factory) error {
+	lock.Lock()
+	defer lock.Unlock()
 	if _, exists := plugins[name]; exists {
 		return fmt.Errorf("extension point %s has already a registered factory", name)
 	}
-	lock.Lock()
 	plugins[name] = f
-	lock.Unlock()
 	return nil
 }
 
